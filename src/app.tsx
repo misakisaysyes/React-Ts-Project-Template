0c40917154b2d8ccda18a3d686445399864d7923
misakisaysyes <misakisaysyes@gmail.com>
2021-11-25 17:34:08 +0800

    fix: lint code

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Router from '@/router'
-import 'antd/dist/antd.less'
+import React from "react"
+import Router from "@/router"
+import "antd/dist/antd.less"
 
 const App = () => {
-    return (
-        <div>
-            REACT-TS-PROJECT-TEMPLATE
-            <Router />
-        </div>
-    )
+	return (
+		<div>
+			REACT-TS-PROJECT-TEMPLATE
+			<Router />
+		</div>
+	)
 }
 
 export default App
